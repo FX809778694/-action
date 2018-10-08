d7cb466e9cbf105db354c10e49b97aa43ec9b213
Thomas <FX809778694@163.com>
2018-10-08 14:21:37 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,17 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './root-reducer';
 
 export default () => {
-  const store = createStore(
-    rootReducer,
-  )
+  let store;
+  //判断是否有Redux调试工具
+  if(!(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())) {
+    store = createStore(
+      rootReducer,
+    )
+  } else {
+    store = createStore(
+      rootReducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  }
   return store
 }
